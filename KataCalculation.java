e5acd5d1cd0bd4833082ed11dfb193d5be78260a
MYR-LXST <148197974+MYR-LXST@users.noreply.github.com>
2023-11-06 17:02:21 +0300

    Add files via upload

---
+++ KataCalculation.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class KataCalculation {
+    public static void main(String[] args){
+        System.out.println("Калькулятор для KataAcademy");
+        System.out.println("Введите значения по примеру --> a + b");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        try {
+            String result = Main.calc(input);
+            System.out.println("Result: " + result);
+        } catch (Error e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+class Main{
+    public static String calc(String input){
+        String[] strings = input.split(" ");
+        if (strings.length !=3) {
+            throw new Error("Не верный формат ввода данных");
+        }
+        int firstNum = Integer.parseInt(strings[0]);
+        int secondNum = Integer.parseInt(strings[2]);
+        char operation = strings[1].charAt(0);
+
+        if (firstNum < 1 || firstNum >10 || secondNum < 1 || secondNum > 10){
+            throw new Error("Числа должны бить от 1 до 10!!!");
+        }
+
+        int result;
+        switch (operation){
+
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                result = firstNum / secondNum;
+                break;
+            default:
+                throw new Error("Не вереый формат ввода данных");
+
+
+        }
+        return Integer.toString(result);
+    }
+}
