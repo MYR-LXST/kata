6f123184bdcd89453c39b1fe65c6a1e70701d213
MYR-LXST <148197974+MYR-LXST@users.noreply.github.com>
2023-11-08 17:48:39 +0300

    Add files via upload

--- KataCalculation.java
+++ KataCalculation.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class KataCalculation {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         System.out.println("Калькулятор для KataAcademy");
         System.out.println("Введите значения по примеру --> a + b");
         Scanner scan = new Scanner(System.in);
@@ -15,17 +16,17 @@ public class KataCalculation {
     }
 }
 class Main{
-    public static String calc(String input){
+    public static String calc(String input) throws IOException{
         String[] strings = input.split(" ");
         if (strings.length !=3) {
-            throw new Error("Не верный формат ввода данных");
+            throw new IOException("Не верный формат ввода данных");
         }
         int firstNum = Integer.parseInt(strings[0]);
         int secondNum = Integer.parseInt(strings[2]);
         char operation = strings[1].charAt(0);
 
         if (firstNum < 1 || firstNum >10 || secondNum < 1 || secondNum > 10){
-            throw new Error("Числа должны бить от 1 до 10!!!");
+            throw new IOException("Числа должны быть от 1 до 10");
         }
 
         int result;
@@ -44,7 +45,7 @@ class Main{
                 result = firstNum / secondNum;
                 break;
             default:
-                throw new Error("Не вереый формат ввода данных");
+                throw new IOException("Не вереый формат ввода данных");
 
 
         }
